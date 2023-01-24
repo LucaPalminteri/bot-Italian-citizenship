d02dbadc9d8414216ecfaca61c956475574d29a4
LucaPalminteri <lucapalminteri02@gmail.com>
2023-01-24 19:53:59 -0300

    change goal: passport

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 let bookCitizenshipBtn = document.getElementsByTagName("a")[24]
 
+// let date = new Date()
+// const {log, clear} = console
+// setInterval(()=> {
+//     clear()
+//     date = new Date()
+//     let hours = date.getHours()
+//     let minutes = date.getMinutes()
+//     let seconds = date.getSeconds()
+//     log(`${hours}:${minutes}:${seconds}`)
+//     if(hours == 20 && minutes == 0 && seconds == 0) {
+//         bookCitizenshipBtn.click()
+//     }
+// },1000)
+
+//#################################################################
+
+const URL = 'https://prenotami.esteri.it/Services/Booking/554'
+
 let date = new Date()
 const {log, clear} = console
 setInterval(()=> {
@@ -10,6 +28,6 @@ setInterval(()=> {
     let seconds = date.getSeconds()
     log(`${hours}:${minutes}:${seconds}`)
     if(hours == 20 && minutes == 0 && seconds == 0) {
-        bookCitizenshipBtn.click()
+        window.location = URL
     }
 },1000)
